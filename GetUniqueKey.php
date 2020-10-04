2f8e870e5a0964da040269e4189b6955fcd6105c
010Hamza010 <60020444+010Hamza010@users.noreply.github.com>
2020-10-04 08:40:13 +0100

    Create GetUniqueKey.php
    If you want to gey a unique name for your photo (and random), this function help. 💯

---
+++ GetUniqueKey.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+      ////////////////////////////////////////////////////////////////////
+     //                                                                //
+    //                Get a unuque key php script                     //
+   //                  Created By: Hamza -__-                        //
+  //                                                                //
+ ////////////////////////////////////////////////////////////////////
+*/
+
+function GetUniqueKey($file_extension, $database, $table, $column, $lenght=60)
+{
+  $str = "abcdefghijklmnopqrstuvwxyz0123456789"; //Write your strings here
+  while (1) {
+      $Key = '';
+      for ($i=0; $i < $lenght; $i++) { 
+        $Key .= $str[mt_rand(0,strlen($str)-1)];
+      }
+      $check = $database->prepare('SELECT * FROM ' . $table . ' WHERE ' . $column . ' = ?'); //Check if the key exist in the database or not
+      $check->execute(array($Key));
+
+      if ($check->rowCount() == 0) {
+        break; //Oh great! new key
+      }
+  }
+  return $Key;
+
+}
+?>
