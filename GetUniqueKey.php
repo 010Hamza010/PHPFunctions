53c6f52d3884de1a582ff505195bca5ab44e5a53
010Hamza010 <60020444+010Hamza010@users.noreply.github.com>
2020-10-04 08:54:16 +0100

    Update GetUniqueKey.php

--- GetUniqueKey.php
+++ GetUniqueKey.php
@@ -17,14 +17,15 @@ function GetUniqueKey($file_extension, $database, $table, $column, $lenght=60)
       for ($i=0; $i < $lenght; $i++) { 
         $Key .= $str[mt_rand(0,strlen($str)-1)];
       }
+      $Name = $Key . "." . $file_extension;
       $check = $database->prepare('SELECT * FROM ' . $table . ' WHERE ' . $column . ' = ?'); //Check if the key exist in the database or not
-      $check->execute(array($Key));
+      $check->execute(array($Name));
 
       if ($check->rowCount() == 0) {
         break; //Oh great! new key
       }
   }
-  return $Key;
+  return $Name;
 
 }
 ?>
